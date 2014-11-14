c8b9109dd655f5c513f4d7d301e28b01b29e78a9
Orfeo Ciano <orfeo18@gmail.com>
2014-11-14 13:11:16 +0100

    * Added Unit Tests samples...

---
+++ Tests/test_position.py
@@ -0,0 +1,38 @@
+import random
+from unittest import TestCase
+
+from Common import Position
+
+
+__author__ = 'Orfeo'
+
+
+class TestInitPosition(TestCase):
+    def setUp(self):
+        self._position = Position()
+
+    def test_x(self):
+        self.assertEquals(self._position.x, 0)
+
+    def test_y(self):
+        self.assertEquals(self._position.y, 0)
+
+    def test_z(self):
+        self.assertEquals(self._position.z, 0)
+
+
+class TestRandomPosition(TestCase):
+    def setUp(self):
+        self._x = random.randint(0, 100)
+        self._y = random.randint(0, 100)
+        self._z = random.randint(0, 100)
+        self._position = Position(self._x, self._y, self._z)
+
+    def test_x(self):
+        self.assertEquals(self._position.x, self._x)
+
+    def test_y(self):
+        self.assertEquals(self._position.y, self._y)
+
+    def test_z(self):
+        self.assertEquals(self._position.z, self._z)
